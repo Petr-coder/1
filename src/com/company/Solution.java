7af6f6482addf59dca2347ad148fac77f437c3dd
Petr <mpshigh@gmail.com>
2020-01-05 16:10:48 +0300

    Initial commit

---
+++ src/com/company/Solution.java
@@ -0,0 +1,17 @@
+package com.company;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+/*ВСоздать class Person. У человека должно быть имя String name, возраст int age.
+Добавь метод initialize(String name, int age), в котором проинициализируй переменные name и age.
+В методе main создай объект Person, занеси его ссылку в переменную person.
+Вызови метод initialize с любыми значениями.*/
+public class Solution {
+
+    public static void main(String[] args) {
+
+        int x=5;
+        int y=6;
+        System.out.printf("%6d%n", x);
+
+    }
+}
