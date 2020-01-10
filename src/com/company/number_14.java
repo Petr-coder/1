ddbe6256c40c3bfec495395819f54c977186ce53
Petr-coder <59264340+Petr-coder@users.noreply.github.com>
2020-01-10 10:20:44 +0300

    Update number_14.java

--- src/com/company/number_14.java
+++ src/com/company/number_14.java
@@ -1,4 +1,22 @@
-package com.company;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+/* Необходимо написать программу, которая будет реализовывать следующие действия:
+1. Ввод числа с клавиатуры и запись его в строковую переменную S
+2. Конвертация строковой переменной S в числовую переменную X типа int
+3. Конвертация числа X типа int в число Y типа double
+Все три числа разных типов необходимо в столбик вывести на экран. */
 
-public class number_14 {
+public class Solution {
+    public static void main(String args[]) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String S = reader.readLine();
+        int X;
+        X = Integer.parseInt(S);
+        double Y;
+        Y = (double) X;
+        System.out.println(S);
+        System.out.println(X);
+        System.out.println(Y);
+    }
 }
