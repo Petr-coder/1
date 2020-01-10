bd50c4820715337397c1297fbfcc184f7731dd57
Petr-coder <59264340+Petr-coder@users.noreply.github.com>
2020-01-10 19:08:18 +0300

    Update number_6.java

--- src/com/company/number_6.java
+++ src/com/company/number_6.java
@@ -1,7 +1,7 @@
-package com.company;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 /*Написать программу, которая будет выполнять следующие действия:
 1. Ввод трех чисел с клавиатуры x, y, z
 2. Нахождение и вывод на экран среднего арифметического чисел x, y, z
@@ -12,13 +12,15 @@ public class number_6 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader mt = new BufferedReader(new InputStreamReader(System.in));
-        int x = Integer.parseInt(mt.readLine());
-        int y = Integer.parseInt(mt.readLine());
-        int z = Integer.parseInt(mt.readLine());
+        double x = Double.parseDouble(mt.readLine());
+        double y = Double.parseDouble(mt.readLine());
+        double z = Double.parseDouble(mt.readLine());
 
-        int a = (x + y + z)/3;
+        double a = (x + y + z)/3;
         System.out.println(a);
         a = a / 2;
+
+
         if (a > 3)
             System.out.println("Программа выполнена корректно");
 
