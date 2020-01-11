734a535790885020982ad8f0c4d10db26c02664e
Petr <mpshigh@gmail.com>
2020-01-11 18:16:21 +0300

    Java № 1

---
+++ src/com/company/number_17.java
@@ -0,0 +1,32 @@
+package com.company;
+import java.io.*;
+/* Необходимо написать программу, которая будет записывать текстовые данные, введенные с экрана, в файл .txt.
+Запись ввести в файл до тех пор, пока не будет введена команда стоп в консоли. Пользователь при тестировании
+программы по правилу черного ящика должен понимать, какой принцип остановки записи в файл он должен подать.
+ */
+
+public class number_17 {
+
+        public static void main(String[] args) {
+            try {
+                InputStreamReader inputStreamReader = new InputStreamReader(System.in); 
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                File file = new File("file.txt");
+                FileWriter fileWriter = new FileWriter(file);
+                BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+                String line;
+                while(!(line = bufferedReader.readLine()).equals("exit")) {
+                    bufferedWriter.write(line);
+                }
+
+                bufferedReader.close();
+                bufferedWriter.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
